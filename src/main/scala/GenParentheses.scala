9fca2162e32fca3335d00725426896d1533d0956
Abhijeet Gaurav <abhijeetgaurav800@gmail.com>
2024-04-26 15:28:52 +0530

    initial commit

--- src/main/scala/GenParentheses.scala
+++ src/main/scala/GenParentheses.scala
@@ -2,7 +2,7 @@ class GenParentheses {
   def generateParenthesis(n: Int): Array[String] = {
     def Helper(open: Int, close: Int, current: String, result: Array[String]): Array[String] = {
       if (open == 0 && close == 0) {
-        (current +: result).reverse
+        (current +: result)
       } else {
         var newResult = result
         if (open > 0) {
@@ -18,4 +18,11 @@ class GenParentheses {
   }
 }
 
-
+object Main {
+  def main(args: Array[String]): Unit = {
+    val n = 4
+    val parentheses = new GenParentheses()
+    val combinations = parentheses.generateParenthesis(n)
+    combinations.foreach(println)
+  }
+}
